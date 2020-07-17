ab78906ad04993292819c1670255c29bb61a9fd8
Sayantan Banerjee <sayantaninfinity16@gmail.com>
2020-07-17 23:40:24 +0530

    permission added

--- app/src/main/java/com/sayantanbanerjee/voicechanger/MainActivity.kt
+++ app/src/main/java/com/sayantanbanerjee/voicechanger/MainActivity.kt
@@ -1,20 +1,29 @@
 package com.sayantanbanerjee.voicechanger
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.media.AudioAttributes
+import android.media.AudioFormat
 import android.media.AudioRecord
 import android.media.AudioTrack
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
+import android.util.Log
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.Spinner
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import java.io.*
-import java.lang.Exception
+
 
 class MainActivity : AppCompatActivity() {
 
-    val file: File = File(Environment.getExternalStorageDirectory(), "test.pcm")
+    private var file: File =
+        File(Environment.getExternalStorageDirectory().absolutePath, "test.pcm")
     private lateinit var spFrequency: Spinner
     private lateinit var record: Button
     private lateinit var play: Button
@@ -39,7 +48,10 @@ class MainActivity : AppCompatActivity() {
         frequencies.add("6050")
         frequencies.add("8500")
         frequencies.add("11025")
+        frequencies.add("14025")
         frequencies.add("16000")
+        frequencies.add("18000")
+        frequencies.add("20000")
         frequencies.add("22050")
         frequencies.add("30000")
         frequencies.add("40000")
@@ -49,13 +61,30 @@ class MainActivity : AppCompatActivity() {
 
         // start record button pressed
         record.setOnClickListener {
-            val intent: Intent = Intent(this, RecordActivity::class.java)
-            startActivity(intent)
             Thread {
                 recording = true
 
                 try {
-                    startRecord()
+                    if (ContextCompat.checkSelfPermission(
+                            this, Manifest.permission.RECORD_AUDIO
+                        ) == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(
+                            this, Manifest.permission.WRITE_EXTERNAL_STORAGE
+                        ) == PackageManager.PERMISSION_GRANTED
+                    ) {
+                        val intent: Intent = Intent(this, RecordActivity::class.java)
+                        startActivity(intent)
+                        startRecord()
+                    } else {
+                        val permissionArrays = arrayOf<String>(
+                            Manifest.permission.RECORD_AUDIO,
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE
+                        )
+                        ActivityCompat.requestPermissions(
+                            this,
+                            permissionArrays,
+                            1
+                        );
+                    }
                 } catch (exception: Exception) {
                     exception.printStackTrace()
                 }
@@ -77,12 +106,36 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 1) {
+            if (ContextCompat.checkSelfPermission(
+                    this, Manifest.permission.RECORD_AUDIO
+                ) == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(
+                    this, Manifest.permission.WRITE_EXTERNAL_STORAGE
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                val intent: Intent = Intent(this, RecordActivity::class.java)
+                startActivity(intent)
+                startRecord()
+            } else {
+                Toast.makeText(this, getString(R.string.denied), Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
     private fun playRecord() {
         var outputFrequency: Int = 0
         val str: String = spFrequency.selectedItem.toString()
         outputFrequency = Integer.parseInt(str)
 
         val shortSizeInBytes = Short.SIZE_BYTES / Byte.SIZE_BYTES
+
+        Log.i("####FILE", file.length().toString())
 
         val bufferedSizeInBytes: Int = (file.length() / shortSizeInBytes).toInt()
         val audioData: ShortArray = ShortArray(bufferedSizeInBytes)
@@ -98,7 +151,8 @@ class MainActivity : AppCompatActivity() {
         }
         dataInputStream.close()
 
-        audioTrack = AudioTrack(3, outputFrequency, 2, 2, bufferedSizeInBytes, 1)
+        // TODO : Here I have hard-coded 10000 as bufferSizeInBytes value whereas it should be "bufferedSizeInBytes" variable
+        audioTrack = AudioTrack(3, outputFrequency, 2, 2, 10000, 1)
         audioTrack.play()
         audioTrack.write(audioData, 0, bufferedSizeInBytes)
 
@@ -119,7 +173,7 @@ class MainActivity : AppCompatActivity() {
 
         while (recording) {
             val numberOfShort = audioRecord.read(audioData, 0, minBufferSize)
-            for (i in 0..numberOfShort) {
+            for (i in 0 until numberOfShort - 1) {
                 dataOutputStream.writeShort(audioData[i].toInt())
             }
         }
